91d1a5156fdf4eb9ee92d105e734c595e35b3744
Fawaz Alhafiz <48890670+fawaaaz111@users.noreply.github.com>
2025-10-02 14:22:26 +0300

    fix: SQL operator precedence in Project query customer filter

--- erpnext/controllers/queries.py
+++ erpnext/controllers/queries.py
@@ -318,8 +318,7 @@ def get_project_name(doctype, txt, searchfield, start, page_len, filters):
 	if filters:
 		if filters.get("customer"):
 			qb_filter_and_conditions.append(
-				(proj.customer == filters.get("customer")) | proj.customer.isnull() | proj.customer == ""
-			)
+				(proj.customer == filters.get("customer")) | (proj.customer.isnull()) | (proj.customer == "")			)
 
 		if filters.get("company"):
 			qb_filter_and_conditions.append(proj.company == filters.get("company"))
